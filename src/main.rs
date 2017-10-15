96e9b75befd6bfc4338db38097263872541f144e
Jonathan Bennett <doulos05@gmail.com>
2017-10-15 18:52:17 +0900

    Check victory function working on single win condition.

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,17 @@ fn main() {
     println!("{}, {}, {}\n{}, {}, {}\n{}, {}, {}",
              cells[0], cells[1], cells[2],
              cells[3], cells[4], cells[5],
-             cells[6], cells[7], cells[8])
+             cells[6], cells[7], cells[8]);
+    cells[0] = 1;
+    cells[1] = 1;
+    cells[2] = 1;
+    println!("Player {} wins!", check_victory(cells));
 }
 
+fn check_victory(cells: [u32; 9]) -> u32 {
+    if cells[0] == cells[1] && cells[1] == cells[2] {
+        cells[0]
+    } else {
+        0
+    }
+}
