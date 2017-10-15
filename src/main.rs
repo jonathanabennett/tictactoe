129bdbae6e06d47171a72dbe253e07c40e80c92b
Jonathan Bennett <doulos05@gmail.com>
2017-10-15 14:18:11 +0900

    Simplified board layout.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ fn main() {
     //Varables
     let mut cells = [0,0,0,0,0,0,0,0,0];
 
-    println!("{}", cells[0])
+    println!("{}, {}, {}\n{}, {}, {}\n{}, {}, {}",
+             cells[0], cells[1], cells[2],
+             cells[3], cells[4], cells[5],
+             cells[6], cells[7], cells[8])
 }
 
