9c2fdb4acd9a15e988be7d7053d88c79a50fcc27
Jonathan Bennett <doulos05@gmail.com>
2017-10-15 18:57:11 +0900

    All win conditions programmed in.

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,20 @@ fn main() {
 fn check_victory(cells: [u32; 9]) -> u32 {
     if cells[0] == cells[1] && cells[1] == cells[2] {
         cells[0]
+    } else if cells[0] == cells[4] && cells[4] == cells[8] {
+        cells[0]
+    } else if cells[0] == cells[3] && cells[3] == cells[6] {
+        cells[0]
+    } else if cells[1] == cells[4] && cells[4] == cells[7] {
+        cells[1]
+    } else if cells[2] == cells[5] && cells[5] == cells[8] {
+        cells[2]
+    } else if cells[3] == cells[4] && cells[4] == cells[5] {
+        cells[3]
+    } else if cells[6] == cells[4] && cells[4] == cells[2] {
+        cells[6]
+    } else if cells[6] == cells[7] && cells[7] == cells[8] {
+        cells[8]
     } else {
         0
     }
