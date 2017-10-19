fa55920cc38895dd5ad94824df871c86c4213b8c
Jonathan Bennett <doulos05@gmail.com>
2017-10-19 18:00:04 +0900

    Switching to enums

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,33 @@
 use std::io;
 
+#[derive(PartialEq)]
+enum CellType {
+    Player1,
+    Player2,
+    Empty,
+}
+
+impl CellType {
+    fn output(self, pos:u8) {
+        match self {
+            CellType::Player1 => 'X',
+            CellType::Player2 => 'O',
+            CellType::Empty =>  pos.to_char()
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 
     //Varables
-    let mut cells: [char; 9] = ['0','1','2','3','4','5','6','7','8'];
-    let mut player: char = 'O';
-    let mut victor: char = 'N';
+    let mut cells: [CellType; 9] = [CellType::Empty, CellType::Empty, CellType::Empty,
+    CellType::Empty, CellType::Empty, CellType::Empty,
+    CellType::Empty, CellType::Empty, CellType::Empty];
+    let mut player = CellType::Player1;
+    let mut victor = CellType::Empty;
 
-    while victor == 'N' {
+    while victor == CellType::Empty {
         print_board(&cells);
         println!("Enter the number of the cell you wish to mark:");
 
@@ -20,38 +39,57 @@ fn main() {
         let cell: usize = cell.trim().parse()
             .expect("Please enter a number from 0-8.");
 
-        change_cell(&mut cells, cell, player);
+        change_cell(&mut cells, player);
+
         print_board(&cells);
         victor = check_victory(cells);
-        if player == 'O' {
-            player = 'X';
+        if player == CellType::Player1 {
+            player = CellType::Player2;
         } else {
-            player = 'O';
+            player = CellType::Player1;
         }
     }
     println!("Player {} wins!", victor);
 }
 
-fn print_board(cells: &[char; 9]) {
+fn print_board(cells: &[CellType; 9]) {
+    let mut output: [char; 9];
+    for cell in cells {
+
     println!("{}, {}, {}\n{}, {}, {}\n{}, {}, {}\n\n\n",
-             cells[0], cells[1], cells[2],
-             cells[3], cells[4], cells[5],
-             cells[6], cells[7], cells[8]);
+             cells[0].output(0), cells[1].output(1), cells[2].output(2),
+             cells[3].output(3), cells[4].output(4), cells[5].output(5),
+             cells[6].output(6), cells[7].output(7), cells[8].output(7));
 }
 
-fn change_cell(cells: &mut [char; 9], cell: usize, player: char) {
-    if cell < 9 {
-        if cells[cell] != 'O' && cells[cell] != 'X' {
-            cells[cell] = player;
+fn change_cell(cells: &mut [CellType; 9], player: CellType) {
+    let mut exit = 0;
+
+    while exit == 0 {
+        println!("Enter the number of the cell you wish to mark:");
+
+        let mut cell = String::new();
+
+        io::stdin().read_line(&mut cell)
+            .expect("Please enter a number from 0-8.");
+
+        let cell: usize = cell.trim().parse()
+            .expect("Please enter a number from 0-8.");
+
+        if cell < 9 {
+            if cells[cell] == CellType::Empty {
+                cells[cell] = player;
+                exit = 1;
+            } else {
+                println!("That cell is already taken!");
+            }
         } else {
-            println!("That cell is already taken!");
+            println!("That's not a valid move! You lose your turn.");
         }
-    } else {
-        println!("That's not a valid move! You lose your turn.");
     }
 }
 
-fn check_victory(cells: [char; 9]) -> char {
+fn check_victory(cells: [CellType; 9]) -> CellType {
     if cells[0] == cells[1] && cells[1] == cells[2] {
         cells[0]
     } else if cells[0] == cells[4] && cells[4] == cells[8] {
@@ -69,6 +107,6 @@ fn check_victory(cells: [char; 9]) -> char {
     } else if cells[6] == cells[7] && cells[7] == cells[8] {
         cells[8]
     } else {
-        'N'
+       CellType::Empty
     }
 }
